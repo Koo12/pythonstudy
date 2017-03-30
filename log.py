e41a0f5fc6a9aae1fa2f4b6fa2a99447b1cdfa43
Your Name <506637238@qq.com>
2017-03-30 23:10:53 +0800

    函数求解

---
+++ log.py
@@ -0,0 +1,13 @@
+import math
+def quadratic(a,b,c):
+	n1=(-b+math.sqrt(b*b-4*a*c))/2*a
+	n2=(-b-math.sqrt(b*b-4*a*c))/2*a
+	return n1,n2
+A=input('输入第一个参数')
+B=input('输入第二个参数')
+C=input('输入第三个三数')
+A=int(A)
+B=int(B)
+C=int(C)
+x1,x2=quadratic(A,B,C)
+print(x1,x2)
