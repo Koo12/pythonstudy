635b200eeceb95ba67e0ee8083a0d496df49dddb
Your Name <506637238@qq.com>
2017-04-03 20:09:32 +0800

    使用generator

--- yanghui.py
+++ yanghui.py
@@ -1,15 +1,15 @@
 def triangles(n):
 	if n==1:
-		print([1])
+		yield [1]
 	if n==2:
-		print([1])
-		print([1,1])
+		yield [1]
+		yield [1,1]
 	if n>=3:
-		print([1])
-		print([1,1])
+		yield [1]
+		yield [1,1]
 		L=[1,2,1]
 		L1=L[:]
-		print(L)
+		yield L
 		c=0
 		while c<n-3:
 			a=1
@@ -19,8 +19,9 @@ def triangles(n):
 				L1[a]=L[b]+L[a]
 				a=a+1
 			L1.append(1)	
-			print(L1)
+			yield L1
 			c=c+1
 N=input('请输入你想要输出几行杨辉三角？')
 N=int(N)
-triangles(N)
+for t in triangles(N):
+	print(t)
